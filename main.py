cc7bdaaf91106a328c907cd8de198ee02dcbda3c
Khush Lalchandani <khush@shubhsoln.com>
2023-04-11 22:38:47 -0500

    Updating User Table

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("hi")
+import mysql.connector
+
+ 
+
+connection = mysql.connector.connect(user = "root", database = "example", password = "Krishna#1")
+cursor = connection.cursor() 
+
+cursor = connection.cursor()
+
+ 
+
+testQuery = ("SELECT * FROM new_table")
+
+ 
+
+cursor.execute(testQuery)
+
+ 
+
+for item in cursor:
+
+    print(item)
+
+ 
+
+cursor.close()
+
+connection.close()
